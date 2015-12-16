ecefb398820bc655b132f837ca1e3cd30e112305
Xiao <b956536180@163.com>
2015-12-16 15:26:28 +0800

    Update lasyman_setup_ss.sh
    Change  to git@github.com:b956536180/ss-panel.git

--- lasyman_setup_ss.sh
+++ lasyman_setup_ss.sh
@@ -232,7 +232,7 @@ function setup_sspanel()
 	PANEL_ROOT=/root/ss-panel
 	echo -e "download ss-panel ...\n"
 	cd /root
-	git clone https://github.com/orvice/ss-panel.git
+	git clone git@github.com:b956536180/ss-panel.git
 	#import pannel sql
 	for mysql in ${SQL_FILES}
 	do
